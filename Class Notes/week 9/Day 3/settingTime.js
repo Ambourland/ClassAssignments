f39d3dc21b87c2444f054518dd2e6b37f9518237
Ambourland <abourland.prsvr@gmail.com>
2024-02-08 20:38:48 -0600

    update week 9 notes

--- Class Notes/week 9/Day 3/settingTime.js
+++ Class Notes/week 9/Day 3/settingTime.js
@@ -1,5 +1,39 @@
-function callback() {
-    let name = "Andrea"
-    console.log(name)
-}
-setInterval(callback, 1000)
+//some interval of time. the setInterval global method take callback function and a duration
+//as a parameter. the duration in milliseconds and the callback will be always called in that
+// interval of time.
+//basic syntax
+
+// function callback() {
+//     let name = "Andrea"
+//     console.log(name)
+// }
+// setInterval(callback, 1000)
+
+// //setting time
+
+// function myName() {
+//     console.log("Andrea")
+// }
+// setTimeout(myName, 2000)
+
+
+// let fruits = ["apple", "orange", "banana", "coconut"]
+
+// fruits.forEach(display);
+// fruits.forEach(lowercase);
+
+// function lowercase(element, index, array) {
+//     array[index] = element.toLowerCase();
+// }
+
+// function upperCase(element, index, array) {
+//     array[index] = element.toUpperCase();
+// }
+
+// function display(element) {
+//     console.log(element);
+// }
+
+let names = ["andrea", "angela", "tara", "cameron", "carlos"]
+
+names.forEach((element) => console.log(element.toUpperCase()))
